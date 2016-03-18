75b5bf72d4ffe9c772901b1fb17c85e5f9b1df51
gopro2027 <gopro2027@gmail.com>
2016-03-18 00:45:17 -0500

    Added base files

--- prx.cpp
+++ prx.cpp
@@ -1,1 +1,16 @@
+//assume all necessary code and files are included from the real menu
+
 #include "Mods.h"
+
+void loop() {
+  //all looped functions go into here
+
+}
+
+void hook() {
+  //this is the hooked function
+  loop();
+  if (menuIsOpen) {
+    //this really doesn't need to be used
+  }
+}
